b412bd30e0f720c69f614dec044a941049870d39
Giselle <kgiselle17@gmail.com>
2021-12-13 17:34:46 -0500

    Se declaro funcion mdLinks

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-// ! Traer funciones (functions.js) para crear funcion MDLink
-const path = require('path');
 const functions = require('./functions');
 
 // ! Funcion principal mdLinks que retornara una promesa
-// const mdLinks = (inputPath, options) =>
-// new Promise ((resolve, reject) => {
-//   validateFile(inputPath)
-//   .then
-// };
+const mdLinks = (inputPath, error) => {
+  const promiseMD = new Promise((resolve, reject) => {
+    if (error) {
+      reject(error);
+    } else {
+      const resMD = functions.asFile(inputPath)
+        .then(() => inputPath)
+        .then((res) => functions.isMarkdown(res))
+        .then((res) => {
+          console.log('*** 3. Es un archivo tipo Markdown', res);
+          return inputPath;
+        })
+        .then((res) => functions.readingFile(res))
+        .then((res) => {
+          console.log('*** 4. Leyendo el archivo');
+          return res;
+        })
+        .then((res) => {
+          console.log('*** 5. Extrayendo Links');
+          functions.getLinks(res);
+        })
+        .catch(() => console.log('Proceso Terminado'));
+      resolve(resMD);
+    }
+  });
+  return promiseMD;
+};
 
+mdLinks('./input-readme/prueba3-readme.md');
 
+module.exports = {
+  mdLinks,
+};
